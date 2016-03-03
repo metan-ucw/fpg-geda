a944ea0ecc211302890e77c8d993872d702886ac
Cyril Hrubis <metan@ucw.cz>
2016-03-03 21:01:19 +0100

    sym: Add more functions to the sym.pm
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ lib/sym.pm
@@ -0,0 +1,154 @@
+#!/usr/bin/perl
+
+package sym;
+
+use strict;
+use warnings;
+
+sub println { print @_, "\n" }
+
+sub line_size
+{
+	my (%ctx) = @_;
+
+	return $ctx{'line_size'} || 10;
+}
+
+sub cap_style
+{
+	my (%ctx) = @_;
+	my %cap_hash = ('square' => 1, 'round' => 2);
+
+	if ($ctx{'cap_style'}) {
+		return $cap_hash{$ctx{'cap_style'}};
+	}
+
+	return 0;
+}
+
+sub fill
+{
+	my (%ctx) = @_;
+
+	return $ctx{'fill'} || 0;
+}
+
+sub text_origin
+{
+	my (%ctx) = @_;
+	my %origin_hash = (
+		"bl" => 0,
+		"ml" => 1,
+		"ul" => 2,
+		"bc" => 3,
+		"mc" => 4,
+		"uc" => 5,
+		"br" => 6,
+		"mr" => 7,
+		"ur" => 8,
+	);
+
+	if ($ctx{'origin'}) {
+		return $origin_hash{$ctx{'origin'}};
+	}
+
+	return 0;
+}
+
+sub text_color
+{
+	my (%ctx) = @_;
+
+	return $ctx{'color'} || 8;
+}
+
+sub circle
+{
+	my ($x, $y, $radius, %ctx) = @_;
+
+	println("V $x $y $radius 3 "
+		. line_size(%ctx) .
+		" 0 0 -1 -1 "
+		. fill(%ctx) .
+		" -1 -1 -1 -1 -1");
+}
+
+# wire connection dot
+sub dot
+{
+	my ($x, $y) = @_;
+
+	sym::circle($x, $y, 25, ("fill" => 1));
+}
+
+sub box
+{
+	my ($x, $y, $w, $h, %ctx) = @_;
+
+	println("B $x $y $w $h 3 "
+		. line_size(%ctx) . " "
+		. cap_style(%ctx) .
+		" 0 -1 -1 "
+		. fill(%ctx) .
+		" -1 -1 -1 -1 -1");
+}
+
+sub line
+{
+	my ($x1, $y1, $x2, $y2, %ctx) = @_;
+
+	println("L $x1 $y1 $x2 $y2 3 "
+		. line_size(%ctx) . " "
+		. cap_style(%ctx) .
+		" 0 -1 -1");
+}
+
+sub text
+{
+	my ($x, $y, $visible, $name_value, $text, %ctx) = @_;
+
+	println("T $x $y "
+		. text_color(%ctx) .
+		" 10 $visible $name_value 0 "
+		. text_origin(%ctx) . " 1");
+	println("$text");
+}
+
+sub pin_text
+{
+	my ($dx, $dy, $name, @pin) = @_;
+
+	my $x = $pin[1] || $dx;
+	my $y = $pin[2] || $dy;
+	my $visible = $pin[3] || 0;
+	my $name_value = $pin[4] || 0;
+	my %ctx = ();
+
+	if (defined $pin[5]) {
+		%ctx = %{$pin[5]};
+	}
+
+	$ctx{"color"} = 5;
+
+	text($x, $y, $visible, $name_value, $name . $pin[0], %ctx);
+}
+
+sub pin
+{
+	my ($x1, $y1, $x2, $y2, $pintype, $pinlabel, $pinnumber, $pinseq) = @_;
+
+	println("P $x1 $y1 $x2 $y2 1 0 0");
+	println("{");
+	pin_text($x1, $y1, "pintype=", @$pintype);
+	pin_text($x1, $y1, "pinlabel=", @$pinlabel);
+	pin_text($x1, $y1, "pinnumber=", @$pinnumber);
+	pin_text($x1, $y1, "pinseq=", @$pinseq);
+	println("}");
+}
+
+sub header
+{
+	println("v 20130925 2");
+}
+
+1;
--- sym/connector/gen.pl
+++ sym/connector/gen.pl
@@ -1,54 +1,12 @@
 #!/usr/bin/perl
 
+use lib '../../lib/';
+use sym;
+
 use strict;
 use warnings;
 
-sub println { print @_, "\n" }
-
-sub line_size
-{
-	my (%context) = @_;
-
-	return $context{'line_size'} || 10;
-}
-
-sub fill
-{
-	my (%context) = @_;
-
-	return $context{'fill'} || 0;
-}
-
-sub gen_circle
-{
-	my ($x, $y, $radius, %ctx) = @_;
-
-	println("V $x $y $radius 3 "
-		. line_size(%ctx) .
-		" 0 0 -1 -1 "
-		. fill(%ctx) .
-		" -1 -1 -1 -1 -1");
-}
-
-sub gen_box
-{
-	my ($x, $y, $w, $h, %ctx) = @_;
-
-	println("B $x $y $w $h 3 "
-		. line_size(%ctx) .
-		" 0 0 -1 -1 "
-		. fill(%ctx) .
-		" -1 -1 -1 -1 -1");
-}
-
-sub gen_line
-{
-	my ($x1, $y1, $x2, $y2, %ctx) = @_;
-
-	println("L $x1 $y1 $x2 $y2 3 " . line_size(%ctx) . " 0 0 -1 -1");
-}
-
-sub gen_pin
+sub pin
 {
 	my ($y, $pin, $label, $type) = @_;
 
@@ -56,36 +14,31 @@ sub gen_pin
 	my $yl = $y + 45;
 
 	if ($type eq "1") {
-		gen_circle(350, $y + 100, 50, ('line_size' => 15));
+		sym::circle(350, $y + 100, 50, ('line_size' => 15));
 	}
 
 	if ($type eq "plug") {
-		gen_box(300, $y + 80, 100, 40, ('fill' => 1));
+		sym::box(300, $y + 80, 100, 40, ('fill' => 1));
 	}
 
 	if ($type eq "socket") {
 		my $yp = $y + 50;
-		println("H 3 15 0 0 -1 -1 0 -1 -1 -1 -1 -1 5");
-		println("M 400,$yp");
-		println("l -50,0");
-		println("c -28,0 -50,22 -50,50");
-		println("c 0,28 22,50 50,50");
-		println("l 50,0");
+		sym::println("H 3 15 0 0 -1 -1 0 -1 -1 -1 -1 -1 5");
+		sym::println("M 400,$yp");
+		sym::println("l -50,0");
+		sym::println("c -28,0 -50,22 -50,50");
+		sym::println("c 0,28 22,50 50,50");
+		sym::println("l 50,0");
 	}
 
-	gen_line(200, $yc, 300, $yc);
+	sym::line(200, $yc, 300, $yc);
 
-	println("P 100 $yc 200 $yc 1 0 0");
-	println("{");
-	println("T 100 $yc 5 10 0 0 0 0 1");
-	println("pintype=pas");
-	println("T 430 $yl 5 10 $label 1 0 0 1");
-	println("pinlabel=$pin");
-	println("T 155 $yc 5 10 0 1 0 6 1");
-	println("pinnumber=$pin");
-	println("T 100 $yc 5 10 0 0 0 0 1");
-	println("pinseq=$pin");
-	println("}");
+	sym::pin(100, $yc, 200, $yc,
+	         ["pas"],
+	         ["$pin", 430, $yl, $label, 1],
+	         ["$pin", 155, $yc, 0, 1, {'origin' => 'br'}],
+		 ["$pin"],
+	);
 }
 
 my $max = 20;
@@ -96,16 +49,13 @@ for my $type (@types) {
 		print("Generating connector-$type-$i.sym...\n");
 		open(my $sym, ">connector-$type-$i.sym") or die $!;
 		select $sym;
-		println("v 20130925 2");
+		sym::header();
 		for (my $p = 0; $p < $i; $p++) {
-			gen_pin(200 * $p, $p + 1, ($i > 2 and ($p == 0 or $p == $i - 1)) ? 1 : 0, $type);
+			pin(200 * $p, $p + 1, ($i > 2 and ($p == 0 or $p == $i - 1)) ? 1 : 0, $type);
 		}
-		println("T -5 243 8 10 0 1 0 0 1");
-		println("device=CONNECTOR_$i");
-		my $y = 200 * $i;
+		sym::text(-5, 243, 0, 1, "device=CONNECTOR_$i");
 		if ($i > 1) {
-			println("T 145 $y 8 10 1 1 0 0 1");
-			println("refdes=J?");
+			sym::text(145, 200 * $i, 1, 1, "refdes=J?");
 		}
 		select STDOUT;
 		close($sym);
