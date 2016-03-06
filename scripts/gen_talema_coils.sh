8b1b896061343d24d9481c0d35bd66520ca704ff
Cyril Hrubis <metan@ucw.cz>
2016-03-06 21:10:19 +0100

    fp: Add more coil sizes.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- scripts/gen_talema_coils.sh
+++ scripts/gen_talema_coils.sh
@@ -16,7 +16,7 @@ prepare $DIR
 #
 # These numbers are taken from gme.cz eshop
 #
-for i in "14 8" "15 8" "15 9" "19 8" "20 8" "20 9" "25 12" "27 12" "30 12" "26 13" "29 13" "41 13" "30 14" "42 16"; do
+for i in "16 7" "14 8" "15 8" "16 8" "15 9" "16 9" "19 8" "20 8" "20 9" "25 12" "27 12" "30 12" "26 13" "29 13" "41 13" "30 14" "42 16"; do
 	SIZE=`echo $i | sed s/\ /_/g`
 	print_gen "coil_talema_$SIZE.fp"
 	../components/gen_coil $i > $ODIR/$DIR/coil_talema_$SIZE.fp
