80b9360cb3a194d1d3419d9a723317f82cba2137
Cyril Hrubis <metan@ucw.cz>
2016-03-06 13:46:31 +0100

    lib/sym.pm: Add warnings
    * Check for non-integers passed as coordinates and sizes
    
    * Check for context keywords
    
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- lib/sym.pm
+++ lib/sym.pm
@@ -7,6 +7,38 @@ use warnings;
 
 sub println { print @_, "\n" }
 
+sub w
+{
+	my $depth = 0;
+	for (; caller $depth; $depth++) {1};
+	my @caller = caller($depth - 1);
+
+	print STDERR "$caller[1]: $caller[2]: ", @_, "\n";
+}
+
+sub check_ctx
+{
+	my (%ctx) = @_;
+	my %keys = ("line_size" => 1, "cap_style" => 1, "dash_style" => 1,
+	            "dash_lenght" => 1, "dash_space" => 1, "color" => 1,
+		    "origin" => 1, "fill" => 1);
+
+	for (keys %ctx) {
+		if (! $keys{$_}) {
+			w("Unexpected value '$_' in context");
+		}
+	}
+}
+
+sub check_int
+{
+	for (@_) {
+		if (!(/^-?\d+$/)) {
+			w("Expected integer not '$_'");
+		}
+	}
+}
+
 sub line_size
 {
 	my (%ctx) = @_;
@@ -89,9 +121,16 @@ sub text_color
 	return $ctx{'color'} || 8;
 }
 
+#
+# Public interface
+#
+
 sub circle
 {
 	my ($x, $y, $radius, %ctx) = @_;
+
+	check_ctx(%ctx);
+	check_int(($x, $y, $radius));
 
 	println("V $x $y $radius 3 "
 		. line_size(%ctx) .
@@ -112,6 +151,9 @@ sub box
 {
 	my ($x, $y, $w, $h, %ctx) = @_;
 
+	check_ctx(%ctx);
+	check_int(($x, $y, $w, $h));
+
 	println("B $x $y $w $h 3 "
 		. line_size(%ctx) . " "
 		. cap_style(%ctx) .
@@ -124,6 +166,9 @@ sub line
 {
 	my ($x1, $y1, $x2, $y2, %ctx) = @_;
 
+	check_ctx(%ctx);
+	check_int(($x1, $y1, $x2, $y2));
+
 	println("L $x1 $y1 $x2 $y2 3 "
 		. line_size(%ctx) . " "
 		. cap_style(%ctx) . " "
@@ -132,9 +177,26 @@ sub line
 		. dash_space(%ctx));
 }
 
+sub hline
+{
+	my ($x1, $x2, $y, %ctx) = @_;
+
+	line($x1, $y, $x2, $y, %ctx);
+}
+
+sub vline
+{
+	my ($x, $y1, $y2, %ctx) = @_;
+
+	line($x, $y1, $x, $y2, %ctx);
+}
+
 sub text
 {
 	my ($x, $y, $visible, $name_value, $text, %ctx) = @_;
+
+	check_ctx(%ctx);
+	check_int(($x, $y));
 
 	println("T $x $y "
 		. text_color(%ctx) .
@@ -159,12 +221,17 @@ sub pin_text
 
 	$ctx{"color"} = 5;
 
+	check_ctx(%ctx);
+	check_int(($x, $y));
+
 	text($x, $y, $visible, $name_value, $name . $pin[0], %ctx);
 }
 
 sub pin
 {
 	my ($x1, $y1, $x2, $y2, $pintype, $pinlabel, $pinnumber, $pinseq) = @_;
+
+	check_int(($x1, $y1, $x2, $y2));
 
 	println("P $x1 $y1 $x2 $y2 1 0 0");
 	println("{");
