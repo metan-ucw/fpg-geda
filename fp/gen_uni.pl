ec452e0e18656f59874793317d7bb2fe5794dd06
Cyril Hrubis <metan@ucw.cz>
2017-06-17 14:23:27 +0200

    fp: Add universal PCB generator.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ fp/gen_uni.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+#
+# Copyright (C) 2009-2016 Cyril Hrubis <metan@ucw.cz>
+#
+# fpg-geda code is distributed under GPLv2+
+#
+# See <http://www.gnu.org/licenses/> for more information
+#
+
+use lib '../lib/';
+use fp;
+
+use strict;
+use warnings;
+
+#
+# Universal PCB patterns
+#
+sub uni
+{
+	my ($x, $y) = @_;
+
+	print("Generating uni_${x}_$y.fp...\n");
+	open(my $fp, ">uni_${x}_$y.fp") or die $!;
+	select $fp;
+	fp::begin("Universal PCB ${x}x$y");
+
+	for (my $i = 0; $i < $x; $i++) {
+		for (my $j = 0; $j < $y; $j++) {
+			my $p = $i + $j * $y;
+			fp::pin_s(100 * $i, 100 * $j, 25, 80, "Pin_$p", $p);
+		}
+	}
+
+	fp::end("Cyril Hrubis");
+	select STDOUT;
+	close($fp);
+}
+
+uni(1, 1);
+
+uni(1, 5);
+uni(2, 5);
+uni(5, 1);
+uni(5, 2);
+uni(5, 5);
+
+uni(1, 10);
+uni(2, 10);
+uni(10, 1);
+uni(10, 2);
+uni(10, 10);
+
+uni(1, 20);
+uni(2, 20);
+uni(20, 1);
+uni(20, 2);
+uni(20, 20);
+
+uni(2, 1);
+uni(1, 2);
+uni(2, 3);
+uni(3, 2);
+uni(1, 3);
+uni(3, 1);
