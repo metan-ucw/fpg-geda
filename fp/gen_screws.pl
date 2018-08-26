b87f53b3c8ba925a54594681b058ae67433558cb
Cyril Hrubis <metan@ucw.cz>
2018-08-26 12:52:45 +0200

    fp: Add screew hole generator
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ fp/gen_screws.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+#
+# Copyright (C) 2009-2016 Cyril Hrubis <metan@ucw.cz>
+#
+# fpg-geda code is distributed under GPLv2+
+#
+# See <http://www.gnu.org/licenses/> for more information
+#
+
+use lib '../lib/';
+use fp;
+
+use strict;
+use warnings;
+
+#
+# Generates holes for metric screws.
+#
+# Variant 1 has 1mm copper ring around the hole
+# Variant 2 has copper of the size of the screew head
+#
+sub hole
+{
+	my ($size, $head_size, $var) = @_;
+
+	print("Generating M${size}-$var.fp...\n");
+	open(my $fp, ">M${size}-$var.fp") or die $!;
+	select $fp;
+
+	fp::begin("M${size}");
+	fp::set_unit("um");
+
+	my $radius = ${size} * 1000 / 2 + 100;
+	my $hs = $head_size * 1000 / 2;
+
+	#fp::circle(0, 0, $radius);
+	fp::hline($radius + 1000, $hs, 0);
+	fp::hline(-$radius - 1000, -$hs, 0);
+	fp::vline(0, $radius + 1000, $hs);
+	fp::vline(0, -$radius - 1000, -$hs);
+
+	my $copper_size = $var == 1 ? ($size+0.5) * 1000 : $head_size * 1000;
+
+	fp::pin_s(0, 0, $size * 1000, $copper_size, "Pin_1", 1);
+
+	fp::end("Cyril Hrubis");
+	select STDOUT;
+	close($fp);
+}
+
+# screew size, head size in mm
+my @sizes = ([1.6, 3.2], [2, 4], [2.5, 5], [3, 6], [3.5, 7], [4, 8], [5, 10], [6, 12], [8, 16], [10, 20]);
+
+for my $i (@sizes) {
+	hole($i->[0], $i->[1], 1);
+	hole($i->[0], $i->[1], 2);
+}
